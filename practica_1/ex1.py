fc7489a38499c8002d5ef118a0a3ab380290059d
MauHdzM <ec2-user@ip-172-31-3-118.us-east-2.compute.internal>
2022-09-27 01:43:27 +0000

    ex1

---
+++ practica_1/ex1.py
@@ -0,0 +1,4 @@
+#First ex "HelloWorld
+import sys
+print("Hello World")
+print(sys.argv[1])
---
+++ practica_1/ex2.py
@@ -0,0 +1,12 @@
+#Ejercicio de Arreglos de 1 a n
+import sys
+a=int(float(sys.argv[1]))
+def arr(a):
+    lista=[range(1,a,1)]
+    print(lista)
+    return 
+arr(a)
+#arr
+#def fierro(lista)
+ #   for for index in range(len(lista)): 
+  #      if abs=1 and index > 1
